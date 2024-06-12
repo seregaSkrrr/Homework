42a1a3ed5f467505aaa39b66ee1c7951bf7942e2
seregaSkrrr <128396175+seregaSkrrr@users.noreply.github.com>
2024-06-12 16:15:33 +0300

    Homework_05_13

---
+++ Homework_05_13/Homework_05_13/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_05_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st = new Student();
+            st.ChangeMark(1, 0, 0);
+            st.Info();
+            st.get_GPA(1);
+        }
+    }
+}
